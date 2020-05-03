23fbee033fc82e3b7cd4ba4de6443a48bd656e7e
Tobias Fiebig <t.fiebig@tudelft.nl>
2020-05-04 00:34:39 +0200

    debug

--- app/controllers/concerns/joiner.rb
+++ app/controllers/concerns/joiner.rb
@@ -92,7 +92,7 @@ module Joiner
     {
       user_is_moderator: false,
       meeting_logout_url: request.base_url + logout_room_path(@room),
-      meeting_recorded: record_setting
+      meeting_recorded: record_setting,
 #      meeting_recorded: true,
       moderator_message: "#{invite_msg}\n\n#{request.base_url + room_path(@room)}",
       host: request.host,
